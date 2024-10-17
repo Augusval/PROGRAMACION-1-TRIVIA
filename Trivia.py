13fbea691a7e43bf34be32da7c62ae46257c221e
Ernesto-L <eleomagno@uade.edu.ar>
2024-10-17 16:47:22 -0300

    pruebajson

--- Trivia.py
+++ Trivia.py
@@ -1,6 +1,6 @@
 import json
 import random
-contenido = open("test.json","r")
+contenido = open("newtest.json","r") #sacar todas las tildes del json o da error
 trivia=contenido.read()
 contenido.close
 arch=json.loads(trivia)
@@ -12,13 +12,13 @@ def SeleccionarPreguntas():  # pregunta cuantas preguntas responder y crea una l
     choice = int(input("1 para 10 preguntas , 2 para 20 preguntas"))
     listarandpreguntas = []
     if  choice == 1:
-        cantqst = 2  # 2 es para testea debe ser 10
+        cantqst = 4  # 2 es para testea debe ser 10
     elif choice ==2:
         cantqst=20
     for i in range (cantqst):
-        rndqst = random.randint(1,2)   # 2 es para test debe ser 25 o variable para poder agregar preguntas
+        rndqst = random.randint(1,4)   # 2 es para test debe ser 25 o variable para poder agregar preguntas
         while rndqst in listarandpreguntas:
-            rndqst = random.randint(1,2)   # aca lo mismo
+            rndqst = random.randint(1,4)   # aca lo mismo
         listarandpreguntas.append(rndqst)
     print(listarandpreguntas)
 
@@ -28,13 +28,13 @@ def SeleccionarPreguntas():  # pregunta cuantas preguntas responder y crea una l
 randomMode = False
 continuar = True
 while continuar:
-    tema=int(input("ingrese 1 para Geografia, 2 para Historia, 3 PARA RANDOM: "))
-    if tema==1:
-        select="Geografia"
-    elif tema==2:
-        select="Historia"
-    elif tema ==3:
+    tema=int(input("ingrese 0 a 6 para tema 8 PARA RANDOM: "))
+    listtema=["Formula","Cine","Futbol","DeporteGeneral","Videojuegos","Autos","CienciasNaturales","CulturaGral","Animales"]
+    if tema ==8:
         randomMode = True
+    else:
+        select=listtema[tema]
+    
 
     cantpreguntas = 10
     listapreguntas=SeleccionarPreguntas()
@@ -44,9 +44,9 @@ while continuar:
         qst = str(i)
 
         if randomMode == True:
-            listtema=["Geografia","Historia"] #abria que agregar todas la categorias
+            listtema=["Literatura","Fromula","Futbol","Deporte General","Video Juegos","Autos","Ciencias Naturales","Cine"] #abria que agregar todas la categorias
             select=listtema[random.randint(0,1)] #1 es para test cambiar
-            qst=str(random.randint(1,2))
+            qst=str(random.randint(1,4))
         else:
             pass
 
