6529880c1c63af462b7a91c0e39e214c2a9b5613
Augusval <89166977+Augusval@users.noreply.github.com>
2024-11-14 12:17:01 -0300

    Update Trivia.py
    Agrego funcionalidad de actualizar el orden de los puntajes de mayor puntaje a menor puntaje

--- Trivia.py
+++ Trivia.py
@@ -95,21 +95,45 @@ def crearpreguntas():
     
 
 def agregarpuntaje(puntajejuego): 
-    print("su puntaje es: ",puntajejuego)
+    print("Su puntaje es:", puntajejuego)
 
-    save = int(input("desea guardar su puntaje 1 si // 2 no: "))
+    save = int(input("¿Desea guardar su puntaje? 1 - Sí / 2 - No: "))
     if save == 1:
         puntajejuego = str(puntajejuego)
         name = input("Ingrese su nombre en 4 letras: ")
         namelen = list(name)
-        while len(namelen) >4:
-            print("nombre invalido")
+        while len(namelen) > 4:
+            print("Nombre inválido")
             name = input("Ingrese su nombre en 4 letras")
             namelen = list(name)
-        puntajes.write(name+" "+puntajejuego)
-        puntajes.write("\n") 
+        
+        # Agrega el nuevo puntaje al archivo
+        with open("puntajes.txt", "a") as puntajes:
+            puntajes.write(name + " " + puntajejuego + "\n")
+        
+        # Ordenar los puntajes después de agregar el nuevo puntaje
+        ordenar_puntajes()
     else:
         pass
+
+def ordenar_puntajes():
+    # Leer todos los puntajes del archivo
+    with open("puntajes.txt", "r") as archivo:
+        puntajes = []
+        for linea in archivo:
+            nombre, puntaje = linea.split()
+            puntajes.append((nombre, int(puntaje)))
+
+    # Ordenar la lista de puntajes de mayor a menor
+    puntajes_ordenados = sorted(puntajes, key=lambda x: x[1], reverse=True)
+
+    # Sobrescribir el archivo con los puntajes ordenados
+    with open("puntajes.txt", "w") as archivo:
+        for nombre, puntaje in puntajes_ordenados:
+            archivo.write(f"{nombre} {puntaje}\n")
+
+    print("Puntajes ordenados y guardados en 'puntajes.txt'.")
+
 
 def maingame(versus):
     continuar = True
